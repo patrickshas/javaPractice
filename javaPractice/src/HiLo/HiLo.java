e15fec2f58ba031074cec043bb58b72f92cee3e3
patrickshas <patrickshas@gmail.com>
2026-03-20 17:29:33 -0400

    HiLo algo updates.

--- javaPractice/src/HiLo/HiLo.java
+++ javaPractice/src/HiLo/HiLo.java
@@ -1,12 +1,21 @@
 package HiLo;
+import java.util.Scanner;
 
 public class HiLo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner scan = new Scanner(System.in);
+		
 		int theNumber = (int)(Math.random() * 100 + 1);
 		System.out.println( theNumber );
+		
+		int guess = 0;
+		
+		System.out.println("Guess a number between 1 and 100");
+		
+		guess = scan.nextInt();
 
 	}
 
