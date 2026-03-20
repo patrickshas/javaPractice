bf475a4cbf89963c729e2aa09767bd50873351dd
patrickshas <patrickshas@gmail.com>
2026-03-20 16:55:06 -0400

    HiLo algo

---
+++ javaPractice/src/javaPractice/HiLo.java
@@ -0,0 +1,12 @@
+package javaPractice;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int theNumber = (int)(Math.random() * 100 + 1);
+
+	}
+
+}
